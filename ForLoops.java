56036b956b44bb3b9cd1958e7aa6e7b9fff2c9a4
Akriti Yadav <72516427+94Akriti@users.noreply.github.com>
2021-12-27 17:55:43 +0530

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,21 @@
+package Project1;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		//int i = 1;
+		//while(i <= n){
+		//		System.out.println(i);
+		//		i++;
+		//}
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println(i);
+		}
+		
+		//System.out.println(i);
+	}
+
+}
