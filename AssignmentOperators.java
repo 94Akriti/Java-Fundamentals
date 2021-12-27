88e9eb085331a6d8eac847138c2a2961d0adb9b1
Akriti Yadav <72516427+94Akriti@users.noreply.github.com>
2021-12-27 17:50:27 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,13 @@
+package Project1;
+
+public class AssignmentOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		a<<=1; // bitwise right shift then assigning a to that value, dec(a) += 10
+		System.out.println(a);
+
+	}
+
+}
---
+++ Precedence.java
@@ -0,0 +1,12 @@
+package Project1;
+
+public class Precedence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10, a = ++i * 5;
+		System.out.println(a);
+
+	}
+
+}
