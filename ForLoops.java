f27b55715d521a11842788769851f5f32b5f3541
Akriti Yadav <72516427+94Akriti@users.noreply.github.com>
2021-12-27 18:08:37 +0530

    Add files via upload

--- ForLoops.java
+++ ForLoops.java
@@ -16,6 +16,18 @@ public class ForLoops {
 		}
 		
 		//System.out.println(i);
+		
+		//Multiple declarations: use logical in conditional block
+		for(int i1 = 1, j1 = 100; i1 <= n && j1 <= 200; i1++, j1+=20) {
+			System.out.println(i1 + " "+ j1);
+		}
+		
+		//Nested for loops
+		for(int i2 = 1; i2 <= n; i2++) {
+			for(int j2 = 1; j2 <= 4; j2++) {
+			System.out.println("i = " + i2 + " j = " + j2);
+			}
+		}
 	}
 
 }
